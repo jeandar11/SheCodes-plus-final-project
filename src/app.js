fa997d0d1d1a5af8dac2458f04298288d8b94ab2
jeandar11 <77243427+jeandar11@users.noreply.github.com>
2021-02-28 14:04:36 -0500

    JS to display city name and local temperature

--- src/app.js
+++ src/app.js
@@ -49,3 +49,26 @@ currentDate.innerHTML = showDate(today);
 
 let currentTime = document.querySelector("#local-time");
 currentTime.innerHTML = showTime(today);
+
+//display city and temperature for searched city
+function showWeather(response) {
+  document.querySelector("h1").innerHTML = response.data.name;
+
+  let temperature = Math.round(response.data.main.temp);
+  document.querySelector("#local-temperature").innerHTML = temperature;
+}
+
+function search(city) {
+  let apiKey = "e272d099b6abcf1dc841d6126369d7ac";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+  axios.get(apiUrl).then(showWeather);
+}
+
+function handleSearch(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  search(city);
+}
+
+let searchForm = document.querySelector("#search-city-form");
+searchForm.addEventListener("submit", handleSearch);
